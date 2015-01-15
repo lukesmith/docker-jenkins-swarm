4b8cde52932af2427140df98475ab9124c28f981
Luke Smith <stuff@lukesmith.net>
2015-01-15 11:40:34 +0000

    Wrap all variables in quotes

--- init.sh
+++ init.sh
@@ -6,11 +6,11 @@ source /etc/environment
 
 /usr/bin/java -jar /opt/jenkins-swarm/swarm-client.jar \
       -fsroot /opt/jenkins-workspace \
-      -master $JENKINS_MASTER_URL \
-      -mode $JENKINS_SWARM_MODE \
-      -executors $JENKINS_SWARM_EXECUTORS \
-      -username $JENKINS_SLAVE_USER \
-      -password $JENKINS_SLAVE_PASSWORD \
-      -name $JENKINS_SWARM_NAME \
+      -master "$JENKINS_MASTER_URL" \
+      -mode "$JENKINS_SWARM_MODE" \
+      -executors "$JENKINS_SWARM_EXECUTORS" \
+      -username "$JENKINS_SLAVE_USER" \
+      -password "$JENKINS_SLAVE_PASSWORD" \
+      -name "$JENKINS_SWARM_NAME" \
       -labels "$JENKINS_SWARM_LABELS" \
       -description "$JENKINS_SWARM_DESCRIPTION"
