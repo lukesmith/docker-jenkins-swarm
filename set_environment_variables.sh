b0748d7a0ed4fb24d5494acf1b5e13b8709aadd8
Luke Smith <stuff@lukesmith.net>
2015-01-16 19:36:36 +0000

    Removed supporting code for JENKINS_WORKSPACE_DIR

--- set_environment_variables.sh
+++ set_environment_variables.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-template='
-{{range $p, $conf := .HostConfig.Binds }}
-  {{ $conf }}
-{{ end }}'
-
-hostbindings=$(docker inspect --format="${template}" $HOSTNAME)
-
-for binding in ${hostbindings// / } ; do
-  myArray=(${binding//:/ })
-  if [ "${myArray[1]}" == "/opt/jenkins-workspace" ]; then
-    hostpath=${myArray[0]}
-    break
-  fi
-done
-
-echo "Discovered host path $hostpath"
-
 if [ "$JENKINS_SWARM_NAME" == "" ] ; then
   name=$(docker inspect --format="{{ .Name }}" $HOSTNAME)
   # Remove the / from the start of the docker name
